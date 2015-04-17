42504df57a822c846e844b140b3e68775ab6ab5c
Robert Hinds <rhinds1004@gmail.com>
2015-04-17 16:36:52 -0700

    commit 1

---
+++ student/student/student.cpp
@@ -0,0 +1,15 @@
+//***************************************************************
+//*  Robert Hinds
+//*  TCES 203
+//*  Assignment #
+//*  
+//*  This Program does
+//***************************************************************
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	return 0;
+}

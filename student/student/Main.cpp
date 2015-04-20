ad1586e434aeb0ce9b26fefc48e6419110201928
rhinds1004 <rhinds1004@gmail.com>
2015-04-19 18:56:22 -0700

    commit test

---
+++ student/student/Main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "student.h"
+using namespace std;
+
+int main()
+{
+	return 0;
+}
--- student/student/student.cpp
+++ student/student/student.cpp
@@ -9,22 +9,7 @@
 
 #include "student.h"
 
-//using namespace std;
 
-
-//int main()
-//{
-//	int count = 0;
-//	std::string placeHolder;
-//	std::cout << "How many student profiles to create:";
-//	std::getline(std::cin, placeHolder);
-//	count = atoi(placeHolder.c_str());
-//	Student * stud = createStudent(count);
-//	changeInfo(stud, 3, 11111);
-//	std::cin.get();
-//	delete[]stud;
-//	return 0;
-//}
 
 int printMenu()
 {
